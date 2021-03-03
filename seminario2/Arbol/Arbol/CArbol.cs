312f101c73d80fa40d445c5a43b5d432b9645043
Omar <omaraneudye@gmail.com>
2021-03-02 22:55:06 -0400

    Arbol añadido

---
+++ seminario2/Arbol/Arbol/CArbol.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol
+{
+    class CArbol
+    {
+        private CNodo raiz;
+        //Este nodo nos apoyará en algunas operaciones
+        private CNodo trabajo;
+        private int i = 0;
+
+        //El constructor nos instanciará el nodo raíz
+        public CArbol()
+        {
+            raiz = new CNodo();
+        }
+
+        public CNodo Insertar(string pDato, CNodo pNodo)
+        {
+            //Si no hay nodo donde insertar, tomamos como si fuera la raíz
+            if(pNodo == null)
+            {
+                raiz = new CNodo();
+                raiz.Dato = pDato;
+
+                //No hay hijo
+                raiz.Hijo = null;
+
+                //No hay hermano
+                raiz.Hermano = null;
+
+                return raiz;
+            }
+
+            //Verificamos si no tiene hijo
+            //Insertamos el dato como hijo
+            if(pNodo.Hijo == null)
+            {
+                CNodo temp = new CNodo();
+
+                temp.Dato = pDato;
+
+                //Conectamos el nuevo nodo como hijo
+                pNodo.Hijo = temp;
+
+                return temp;
+            }
+
+            else //Ya tiene un hijo, tenemos que insertarlo como hermano
+            {
+                trabajo = pNodo.Hijo;
+
+                //Avanzamos hasta el último hermano
+                while(trabajo.Hermano != null)
+                {
+                    trabajo = trabajo.Hermano;
+                }
+
+                //Creamos nodo temporal
+                CNodo temp = new CNodo();
+
+                temp.Dato = pDato;
+
+                //Unimos el temp al último hermano
+                trabajo.Hermano = temp;
+
+                return temp;
+            }
+
+            //return raiz;
+        }
+
+
+    }
+}
---
+++ seminario2/Arbol/Arbol/CNodo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol
+{
+    class CNodo
+    {
+        //El dato o los datos que guardará el nodo
+        private string dato;
+
+        //Las referencias a nodo hijo y hermano
+        private CNodo hijo;
+        private CNodo hermano;
+
+        //propiedades de los datos del nodo, de los hijos y hermanos
+        public string Dato { get => dato; set => dato = value; }
+        public CNodo Hijo { get => hijo; set => hijo = value; }
+        public CNodo Hermano { get => hermano; set => hermano = value; }
+
+        //El constructor del nodo
+        public CNodo()
+        {
+            dato = "";
+            hijo = null;
+            hermano = null;
+        }
+    }
+}
