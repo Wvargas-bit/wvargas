4e10b34738c6e8e9a66e98bf674397a489d13e11
201802976 <70177394+201802976@users.noreply.github.com>
2021-03-19 16:48:07 -0400

    Add files via upload
    Algoritmo Dijkstra

---
+++ Dijkstra/Dijkstra/Dijkstra.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra
+{
+    class Dijkstra
+    {
+        private int rango = 0;
+        private int [,] L;
+        private int[] C;
+        public int[] D;
+        private int trango = 0;
+
+        public Dijkstra(int paramRango, int [,]paramArreglo)
+        {
+            L = new int[paramRango, paramRango];
+            C = new int[paramRango];
+            D = new int[paramRango];
+            rango = paramRango;
+
+            for(int i = 0; i <rango; i++)
+            {
+                for(int j = 0; j < rango; j++)
+                {
+                    L[i, j] = paramArreglo[i, j];
+                }
+            }
+
+            for(int i = 0; i < rango; i++)
+            {
+                C[i] = i;
+            }
+
+            C[0] = -1;
+
+            for(int i = 1; i < rango; i++)
+            {
+                D[i] = L[0, i];
+            }
+        }
+
+        public void solDijkstra()
+        {
+            int minValor = Int32.MaxValue;
+            int minNodo = 0;
+
+            for(int i = 0; i < rango; i++)
+            {
+                if(C[i] == -1)
+                {
+                    continue;
+                }
+
+                if(D[i] > 0 && D[i] < minValor)
+                {
+                    minValor = D[i];
+                    minNodo = i;
+                }
+                
+            }
+
+            C[minNodo] = -1;
+
+            for(int i = 0; i < rango; i++)
+            {
+                if(L[minNodo, i] < 0)
+                {
+                    continue;
+                }
+
+                if (D[i] < 0)
+                {
+                    D[i] = minValor + L[minNodo, i];
+                    continue;
+                }
+
+                if ((D[minNodo] + L[minNodo, i]) < D[i])
+                {
+                    D[i] = minValor + L[minNodo, i];
+                }
+            }
+        }
+
+        public void correrDijkstra ()
+        {
+            for(trango = 1; trango < rango; trango++)
+            {
+                solDijkstra();
+                Console.WriteLine("Iteracion no." + trango);
+                Console.WriteLine("Matriz de distancias: ");
+
+                for(int i = 0; i < rango; i++)
+                {
+                    Console.Write(i + " ");  
+                }
+
+                Console.WriteLine(" ");
+
+                for (int i = 0; i < rango; i++)
+                {
+                    Console.Write(D[i] + " ");
+                }
+
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Dijkstra/Dijkstra/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[,] L = {
+                {-1, 10, 18, -1, -1, -1, -1},
+                {-1, -1, 6, -1, 3, -1, -1},
+                {-1, -1, -1, 3, -1, 20, -1},
+                {-1, -1, 2, -1, -1, -1, 2},
+                {-1, -1, -1, 6, -1, -1, 10},
+                {-1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, 10, -1, -1, 5, -1},
+            };
+
+            Dijkstra dijkstra = new Dijkstra((int)Math.Sqrt(L.Length), L);
+
+            dijkstra.correrDijkstra();
+            Console.WriteLine(
+                "La solucion de la ruta mas corta tomando como nodo incial el nodo 1 es: "
+                );
+            int nodo = 1;
+
+            foreach( int i in dijkstra.D)
+            {
+                Console.Write("Distancia minima a nodo " + nodo + " es");
+                Console.WriteLine(i);
+                nodo++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Presione la tecla Enter para salir. ");
+            Console.Read();
+        }
+    }
+}
